910a7258899afa59cc646dfc4db02dd7e3f98b49
Sunghwan Park <shoark7@gmail.com>
2017-07-25 23:57:01 +0900

    Create member model and manager

--- image_tank/settings.py
+++ image_tank/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'member',
+    # 'image',
 ]
 
 MIDDLEWARE = [
@@ -100,12 +103,16 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
+# User model
+AUTH_USER_MODEL = 'member.Member'
+
+
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ko-kr'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
@@ -118,3 +125,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
--- member/models.py
+++ member/models.py
@@ -1,3 +1,41 @@
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 from django.db import models
 
-# Create your models here.
+
+class MemberManager(BaseUserManager):
+    def create_user(self, member_id, name, password, email=None):
+        member_id = self.model.normalize_username(member_id)
+        user = self.model(member_id=member_id,
+                          name=name,
+                          email=email,)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, member_id, name, password, email=None):
+        user = self.create_user(member_id, name, password, email)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
+        return user
+
+
+class Member(AbstractBaseUser, PermissionsMixin):
+    member_id = models.CharField(max_length=20, unique=True)
+    name = models.CharField(max_length=20)
+    email = models.EmailField(max_length=40, blank=True, null=True)
+    is_staff = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'member_id'
+    REQUIRED_FIELDS = ['name',]
+
+    objects = MemberManager()
+
+    def __str__(self):
+        return self.member_id
+
+    def get_short_name(self):
+        return self.__str__()
+
+    def get_full_name(self):
+        return self.member_id + ": " + self.name
